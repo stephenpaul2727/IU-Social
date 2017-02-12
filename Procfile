cc5974004cd9b01815aa3cb58ea8386a2f7753b3
stephenpaul2727 <stephenpaul2727@gmail.com>
2017-02-12 15:20:08 -0500

    Initial Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JAVA_OPTS -Dserver.port=$PORT -jar target/iusocial-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/com/iusocial/IusocialApplication.java
@@ -0,0 +1,12 @@
+package com.iusocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IusocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IusocialApplication.class, args);
+	}
+}
